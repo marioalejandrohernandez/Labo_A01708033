b7313662a124c97ad77d3f08f93de13a97ac561e
marioalejandrohernandez <102115208+marioalejandrohernandez@users.noreply.github.com>
2023-11-15 19:40:51 -0600

    Clase 2 (1).py

--- Clase 2 (1).py
+++ Clase 2 (1).py
@@ -2,19 +2,21 @@ import datetime
 import time 
 import pandas as pd
 import streamlit as st
-from PIL import image
+from PIL import Image  # "Image" en lugar de "image"
 
-st.tittle("Titulo: Analitica de Datos")
+st.title("Titulo: Analitica de Datos")  # "title" en lugar de "tittle"
 st.header("Este es un header")
 st.subheader("Este es un subheader")
 
-st.text("Texto:Hola Streamlit")
-st.markdow("#Este es un matkdown h1 \n  ##Este es un h2 \n ### Este es un h3 ")t
-st.succes("Succesful!")
+st.text("Texto: Hola Streamlit")
+st.markdown("# Este es un markdown h1 \n ## Este es un h2 \n ### Este es un h3 ")  # Corregir "matkdown" a "markdown"
+st.success("Succesful!")  # Corregir "succes" a "success"
 st.info("Información")
-st.warning("warning")
+st.warning("Warning")  # "Warning" con mayúscula
 st.error("Error")
-st.excepetion("Name Error (No esta definido)")
+# st.exception("Name Error (No esta definido)")  # No hay función "exception" en Streamlit
 st.header("Obtener informacion de ayuda de Python")
 st.help(range)
 st.header("Widget")
+
+
