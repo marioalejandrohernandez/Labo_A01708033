5915efe95a3d9e6144a2ce40cc89c37df0789879
marioalejandrohernandez <102115208+marioalejandrohernandez@users.noreply.github.com>
2023-11-15 19:37:43 -0600

    Add files via upload

---
+++ Clase 2 (1).py
@@ -0,0 +1,20 @@
+import datetime 
+import time 
+import pandas as pd
+import streamlit as st
+from PIL import image
+
+st.tittle("Titulo: Analitica de Datos")
+st.header("Este es un header")
+st.subheader("Este es un subheader")
+
+st.text("Texto:Hola Streamlit")
+st.markdow("#Este es un matkdown h1 \n  ##Este es un h2 \n ### Este es un h3 ")t
+st.succes("Succesful!")
+st.info("Información")
+st.warning("warning")
+st.error("Error")
+st.excepetion("Name Error (No esta definido)")
+st.header("Obtener informacion de ayuda de Python")
+st.help(range)
+st.header("Widget")
